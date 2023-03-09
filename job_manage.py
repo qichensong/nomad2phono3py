68bb1ab4d6429a5f6af6b0a0c9fa20c0df3644cc
RyotaroOkabe <50738851+RyotaroOKabe@users.noreply.github.com>
2023-03-08 19:01:02 -0500

    Update job_manage.py

--- job_manage.py
+++ job_manage.py
@@ -77,10 +77,45 @@ def managing_job(workdir0,jobid):
 		x = 600
 		time.sleep(x)
 
+def abo_done(workdir, idx):
+    abo = os.path.join(workdir, ("disp-{0:05d}.abo".format(idx)))
+    f = open(abo,'r')
+    lines = f.readlines()
+    for x in ['', '\n', ' \n']:
+        if x in lines:
+            lines.remove(x)
+    # return lines[-1].startswith('+Overall')
+    return re.search("Overall time",lines[-1])
+
+
+def screen_incomplete(workdir0, jobid, run_job=False):
+    workdir = os.path.join(workdir0,jobid)
+    # keep monitoring
+    dirs=glob.glob(os.path.join(workdir,"supercell-*.in"))
+    # total number of displacements that need to be computed
+    ndisp = len(dirs) 
+    incomplete_list=[]
+    for idx in range(1, ndisp+1):
+        if not abo_done(workdir, idx):
+            print("disp-{0:05d}.abo is not completed".format(idx))
+            incomplete_list.append(idx)
+            if run_job:
+                f = open(os.path.join(workdir,"run.sh"),'r')
+                lines = f.readlines()
+                f.close()
+                lines[i]=("for i in {{{0:05d}..{1:05d}}}\n".format(idx,idx))
+                f = open(os.path.join(workdir,"run.sh"),'w')
+                f.writelines(lines)
+                f.close()
+                os.system('sbatch run.sh')
+    print('incomplete: ', incomplete_list)
+		
 
 if __name__ == "__main__":
-	managing_job('/work2/09337/qcsong/frontera/nomad2phono3py/jobs/','1000_1')	 
-	
+	jobdir = '/work2/09337/qcsong/frontera/nomad2phono3py/jobs/'
+	mpid = '149'
+	managing_job(jobdir,mpid+'_1')	 
+    	screen_incomplete(jobdir, f'{mpid}_1', run_job=True)
 
 
 
