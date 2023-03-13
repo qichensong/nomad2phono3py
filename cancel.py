74c17a852c7c772096f01b1eb59c9e9ee0308798
Ryotaro Okabe <ryotaro@login3.frontera.tacc.utexas.edu>
2023-03-13 16:41:58 -0500

    cancel code add

--- cancel.py
+++ cancel.py
@@ -1,15 +1,17 @@
 import os
 import argparse
 
-def cancel_jobs(start, end)
-for i in range(start, end+1):
-    os.system(f'scancel {i}')
+def cancel_jobs(start, end):
+    for i in range(int(start), int(end)+1):
+        print(f'scancel {i}')
+        os.system(f'scancel {i}')
 
 def main(args):
-    cancel_jobs(args.start, args.end)
+    cancel_jobs(args.fr, args.to)
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--start', required=True)
-    parser.add_argument('--end', required=True)
+    parser.add_argument('--fr', required=True)
+    parser.add_argument('--to', required=True)
     args = parser.parse_args()
+    main(args)
